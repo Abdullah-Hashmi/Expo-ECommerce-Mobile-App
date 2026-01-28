e268f351b024dbfcd2299647526c0d9708cb0d6d
Syed Abdullah <syedabdullah.developer@gmail.com>
2026-01-28 17:16:01 +0500

    set .env

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,14 @@ import express from "express";
 
 const app = express();
 
-app.get("/api/health", (req,res)=>{
-    res.status(200).json({message:"success"});
+// app.use(express.json()); // allows reading req.body JSON
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ message: "success" });
 });
 
-const PORT=3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>{
-    console.log("Server is up and running")
-})
+app.listen(PORT, () => {
+  console.log(`Server is up and running on http://localhost:${PORT}`);
+});
